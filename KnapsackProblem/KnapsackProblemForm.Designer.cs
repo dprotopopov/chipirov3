a7126e3ca90bc09fbcaaaa5faeab1287b60344d4
dprotopopov <protopopov@narod.ru>
2016-03-20 15:33:22 +0300

    first commit

---
+++ KnapsackProblem/KnapsackProblemForm.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace KnapsackProblem
+{
+    partial class KnapsackProblemForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDownCapacity = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCapacity)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDownCapacity
+            // 
+            this.numericUpDownCapacity.DecimalPlaces = 3;
+            this.numericUpDownCapacity.Location = new System.Drawing.Point(263, 42);
+            this.numericUpDownCapacity.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDownCapacity.Name = "numericUpDownCapacity";
+            this.numericUpDownCapacity.Size = new System.Drawing.Size(567, 26);
+            this.numericUpDownCapacity.TabIndex = 0;
+            this.numericUpDownCapacity.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(97, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Вместимость";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(263, 89);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(567, 280);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Вес";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Цена";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Вкл";
+            this.Column3.Name = "Column3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(101, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Предметы";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(105, 439);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Итоговый вес";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(984, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 37);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Загрузить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.openFile_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(984, 89);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 37);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Сохранить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.saveFile_Click);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.DecimalPlaces = 2;
+            this.numericUpDown2.Location = new System.Drawing.Point(263, 439);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.ReadOnly = true;
+            this.numericUpDown2.Size = new System.Drawing.Size(567, 26);
+            this.numericUpDown2.TabIndex = 9;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.DecimalPlaces = 2;
+            this.numericUpDown3.Location = new System.Drawing.Point(263, 471);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.ReadOnly = true;
+            this.numericUpDown3.Size = new System.Drawing.Size(567, 26);
+            this.numericUpDown3.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(105, 471);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(121, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Итоговая цена";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "txt";
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "txt";
+            this.saveFileDialog1.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(984, 181);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 75);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Полный перебор";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.bruteForce_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 3;
+            this.numericUpDown1.Location = new System.Drawing.Point(263, 387);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(136, 26);
+            this.numericUpDown1.TabIndex = 13;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.DecimalPlaces = 2;
+            this.numericUpDown4.Location = new System.Drawing.Point(405, 387);
+            this.numericUpDown4.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(136, 26);
+            this.numericUpDown4.TabIndex = 14;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(560, 381);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(120, 37);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Добавить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // KnapsackProblemForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1149, 565);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDownCapacity);
+            this.Name = "KnapsackProblemForm";
+            this.Text = "Задача о рюкзаке";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCapacity)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDownCapacity;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Column3;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ KnapsackProblem/KnapsackProblemForm.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KnapsackProblem
+{
+    public partial class KnapsackProblemForm : Form
+    {
+        public KnapsackProblemForm()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            UpdateTotal();
+        }
+
+        private void UpdateTotal()
+        {
+            var totalWeight = 0.0;
+            var totalPrice = 0.0;
+            for (var index = 0; index < dataGridView1.Rows.Count; index++)
+            {
+                if ((bool) dataGridView1[2, index].EditedFormattedValue)
+                {
+                    totalWeight += Convert.ToDouble(dataGridView1[0, index].EditedFormattedValue);
+                    totalPrice += Convert.ToDouble(dataGridView1[1, index].EditedFormattedValue);
+                }
+            }
+            numericUpDown2.Value = (decimal) totalWeight;
+            numericUpDown3.Value = (decimal) totalPrice;
+        }
+
+        /// <summary>
+        ///     Сохранение файла с параметрами задачи
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void saveFile_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() != DialogResult.OK) return;
+            using (var writer = File.CreateText(saveFileDialog1.FileName))
+            {
+                writer.WriteLine(numericUpDownCapacity.Value);
+                for (var index = 0; index < dataGridView1.Rows.Count; index++)
+                {
+                    writer.WriteLine(dataGridView1[0, index].EditedFormattedValue + ";" +
+                                     dataGridView1[1, index].EditedFormattedValue);
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Чтение файла с параметрами задачи
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void openFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() != DialogResult.OK) return;
+            using (var reader = File.OpenText(openFileDialog1.FileName))
+            {
+                dataGridView1.Rows.Clear();
+                numericUpDownCapacity.Value = Convert.ToDecimal(reader.ReadLine());
+                for (var line = reader.ReadLine(); line != null; line = reader.ReadLine())
+                {
+                    var array = line.Split(';').Cast<object>().ToArray();
+                    dataGridView1.Rows.Add(array);
+                }
+            }
+            UpdateTotal();
+        }
+
+        /// <summary>
+        /// Нахождение решения полным перебором
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void bruteForce_Click(object sender, EventArgs e)
+        {
+            var capacity = (double) numericUpDownCapacity.Value;
+            var weights = new double[dataGridView1.Rows.Count];
+            var prices = new double[dataGridView1.Rows.Count];
+            var foundedPrice = 0.0;
+            var foundedIndex = 0L;
+            var mutex = new Mutex();
+
+            for (var index = 0; index < dataGridView1.Rows.Count; index++)
+            {
+                weights[index] = Convert.ToDouble(dataGridView1[0, index].EditedFormattedValue);
+                prices[index] = Convert.ToDouble(dataGridView1[1, index].EditedFormattedValue);
+            }
+
+            if (dataGridView1.Rows.Count > 30)
+            {
+                MessageBox.Show("Очень долго");
+                return;
+            }
+
+            // Вычисляем параллельно для всех комбинаций элементов
+            Parallel.For(0L, 1L << dataGridView1.Rows.Count, bits =>
+            {
+                var weight = 0.0;
+                var price = 0.0;
+                var index = 0;
+                for (var j = bits; j > 0; j >>= 1)
+                {
+                    if ((j & 1) == 1)
+                    {
+                        weight += weights[index];
+                        price += prices[index];
+                    }
+                    index++;
+                }
+                if (weight > capacity) return;
+                mutex.WaitOne();
+                if (price >= foundedPrice)
+                {
+                    foundedPrice = price;
+                    foundedIndex = bits;
+                }
+                mutex.ReleaseMutex();
+            });
+
+            for (var index = 0; index < dataGridView1.Rows.Count; index++, foundedIndex >>= 1)
+            {
+                dataGridView1[2, index].Value = ((foundedIndex & 1) == 1);
+            }
+            UpdateTotal();
+        }
+
+        /// <summary>
+        /// Добавление одного предмета указанного веса и цены
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button4_Click(object sender, EventArgs e)
+        {
+            object[] array = {numericUpDown1.Value, numericUpDown4.Value};
+            dataGridView1.Rows.Add(array);
+        }
+    }
+}
